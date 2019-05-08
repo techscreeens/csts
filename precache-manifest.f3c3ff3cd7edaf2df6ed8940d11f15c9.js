6ee5972ec21897627408d8880d318c6b02f5b608
techscreens <techscreens@example.com>
2019-05-08 08:17:59 +0100

    Initial gh-pages deploy

---
+++ precache-manifest.f3c3ff3cd7edaf2df6ed8940d11f15c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbcdd3a9d3737d3b6ffae3f3a6fc8c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b780984e4cf0f1f0562",
+    "url": "/static/css/main.a36e573e.chunk.css"
+  },
+  {
+    "revision": "ea9ecd2e608cf00dba15",
+    "url": "/static/js/2.87b57af5.chunk.js"
+  },
+  {
+    "revision": "5b780984e4cf0f1f0562",
+    "url": "/static/js/main.6d869e0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be162ae100b78a82b394ad843a3e69c9",
+    "url": "/static/media/logo.be162ae1.png"
+  }
+]);
